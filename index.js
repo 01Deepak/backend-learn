e00f1e4ee7424b06bc20a79b3e2b19ccd18c1860
Deepak Singh <deepak7275759723@gmail.com>
2025-05-25 00:43:03 +0530

    multiple data export import

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sum = require('./modules/sum.js')
+const math = require('./modules/math.js')
 
 console.log(sum(7,3));
-
+console.log(math.add(2,3))
+console.log(math.sub(7,5))
 console.log("Learning modules.")
